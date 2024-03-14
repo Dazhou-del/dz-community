d13639022fbb23c12f2ebadc3a699f503c985af4
Dazhou-del <1797050390@qq.com>
2024-03-14 23:24:59 +0800

    [+] 添加mvc mysql mybatis等依赖

---
+++ subject/subject-application/subject-application-controller/src/main/java/com/dazhou/subject/application/controller/SubjectController.java
@@ -0,0 +1,26 @@
+package com.dazhou.subject.application.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.dazhou.subject.infra.basic.entity.SubjectCategory;
+import com.dazhou.subject.infra.basic.service.SubjectCategoryService;
+import com.dazhou.subject.infra.basic.service.impl.SubjectCategoryServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author <a href="https://github.com/Dazhou-del">Dazhou</a>
+ * @create 2024-03-14 21:29
+ */
+@RestController
+public class SubjectController {
+    @Resource
+    private SubjectCategoryServiceImpl subjectCategoryService;
+    @GetMapping("/test")
+    private String test(){
+        LambdaQueryWrapper<SubjectCategory> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(SubjectCategory::getId, 1L);
+        return subjectCategoryService.getOne(queryWrapper).getCategoryName();
+    }
+}
--- subject/subject-starter/src/main/java/com/dazhou/subject/SubjectApplication.java
+++ subject/subject-starter/src/main/java/com/dazhou/subject/SubjectApplication.java
@@ -1,7 +1,9 @@
 package com.dazhou.subject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 刷题微服务启动类
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2024-03-14 21:19
  */
 @SpringBootApplication
+@ComponentScan("com.dazhou")
+@MapperScan("com.dazhou.**.mapper")
 public class SubjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(SubjectApplication.class);
