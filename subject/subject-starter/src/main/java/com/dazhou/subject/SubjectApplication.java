050640030d1f3680b7d1a10d4e13a271c7a6ee56
Dazhou-del <1797050390@qq.com>
2024-03-14 21:26:10 +0800

    [+] 添加启动类

---
+++ subject/subject-starter/src/main/java/com/dazhou/subject/SubjectApplication.java
@@ -0,0 +1,16 @@
+package com.dazhou.subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 刷题微服务启动类
+ * @author <a href="https://github.com/Dazhou-del">Dazhou</a>
+ * @create 2024-03-14 21:19
+ */
+@SpringBootApplication
+public class SubjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectApplication.class);
+    }
+}
