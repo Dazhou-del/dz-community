a0ffb01915a2e378981d4d63ceb09290cce47f48
Dazhou-del <1797050390@qq.com>
2024-03-18 23:06:26 +0800

    [+] 剩余subject的基本结构

---
+++ subject/subject-infra/src/main/java/com/dazhou/subject/infra/basic/service/SubjectJudgeService.java
@@ -0,0 +1,13 @@
+package com.dazhou.subject.infra.basic.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import generator.domain.SubjectJudge;
+
+/**
+* @author da zhou
+* @description 针对表【subject_judge(判断题)】的数据库操作Service
+* @createDate 2024-03-18 22:32:31
+*/
+public interface SubjectJudgeService extends IService<SubjectJudge> {
+
+}
---
+++ subject/subject-infra/src/main/java/com/dazhou/subject/infra/basic/service/SubjectMappingService.java
@@ -0,0 +1,13 @@
+package com.dazhou.subject.infra.basic.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import generator.domain.SubjectMapping;
+
+/**
+* @author da zhou
+* @description 针对表【subject_mapping(题目分类关系表)】的数据库操作Service
+* @createDate 2024-03-18 22:32:57
+*/
+public interface SubjectMappingService extends IService<SubjectMapping> {
+
+}
